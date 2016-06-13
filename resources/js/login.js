8b9e481a7bc30a54ad3243545ff19d46ae4fc0b2
dijalmasilva <dijalmacz@gmail.com>
2016-06-13 15:33:03 -0300

    Comentando e alterando a rolagem da barra

--- resources/js/login.js
+++ resources/js/login.js
@@ -10,10 +10,11 @@ $(document).ready(function () {
     animationend = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
     var posicaoScroll = 0;
     valorIconDown = -10;
+    widthTela = $(window).width();
+    console.log(widthTela);
 
     $(window).scroll(function () {
         posicaoScroll = $(this).scrollTop();
-        console.log(posicaoScroll);
 
 //        Função para rolagem do header
         if (posicaoScroll === 0) {
@@ -42,6 +43,52 @@ $(document).ready(function () {
             }, 0);
         }
 
+        //verifica se o dispositivo é maior que um tablet
+        if (widthTela > 768){
+            //efeito do iescolar
+            if (posicaoScroll > 150) {
+                $('#iescolar').addClass('animated rotateInDownLeft');
+            }
+
+            //efeito das funções 
+            if (posicaoScroll > 490) {
+                $('.effect').addClass('animated bounceInUp');
+            }
+
+            //efeito redes sociais
+            if (posicaoScroll > 1100) {
+                $('.row_custom').addClass('animated zoomIn');
+            }
+
+            //efeito coruja rodapé
+            if (posicaoScroll > 1600) {
+                $('.icon-footer').addClass('animated slideInLeft');
+            }
+        //verifica se é um tablet
+        }else if (widthTela > 425){
+             //efeito do iescolar
+            if (posicaoScroll > 120) {
+                $('#iescolar').addClass('animated rotateInDownLeft');
+            }
+
+            //efeito das funções 
+            if (posicaoScroll > 490) {
+                $('.effect').addClass('animated bounceInUp');
+            }
+
+            //efeito redes sociais
+            if (posicaoScroll > 1100) {
+                $('.row_custom').addClass('animated zoomIn');
+            }
+
+            //efeito coruja rodapé
+            if (posicaoScroll > 1600) {
+                $('.icon-footer').addClass('animated slideInLeft');
+            }
+        }
+
+        //faz a troca entre os botões de rolagem
+
         //botaoTopo
         if (posicaoScroll > 150){
             $('#descer').addClass('invisible');
@@ -58,45 +105,30 @@ $(document).ready(function () {
             $('#descer').addClass('animated fadeInUp');
         }
 
-        //efeito do iescolar
-        if (posicaoScroll > 150) {
-            $('#iescolar').addClass('animated rotateInDownLeft');
-        }
-
-//        efeito das funções 
-        if (posicaoScroll > 490) {
-            $('.effect').addClass('animated bounceInUp');
-        }
-
-        //efeito redes sociais
-        if (posicaoScroll > 1100) {
-            $('.row_custom').addClass('animated zoomIn');
-        }
-
-        //efeito coruja rodapé
-        if (posicaoScroll > 1600) {
-            $('.icon-footer').addClass('animated slideInLeft');
-        }
-
     });
     
+    //Verifica se o efeito do modal login finalizou
     $('.modal_conteudo').one(animationend, function (){
+        //remove os efeitos
        $(this).removeClass('animated zoomInUp');
     });
     
+    //fecha o modal de login clicando no x
     $('#x').click(function (){
         fecharModalLogin();   
     });
-
+    
+    //fecha o modal de login apertando a tecla ESC
     $('body').keyup(function(event) {
-        /* Act on the event */
+        //Compara se a tecla apertada é igual ao código de ESC
         if (event.which == 27){
             fecharModalLogin();
         }
     });
 
+    //Função para rolar a página até o topo
     $('#voltarAoTopo').click(function(event) {
-        /* Act on the event */
+        //Faz a função de rolagem em 1 segundo
         $('html, body').animate({
             scrollTop: '0'},
             1000, function() {
@@ -104,22 +136,37 @@ $(document).ready(function () {
         });
     });
 
+    //Função para rolar a página até o ponto especificado
     $('#descer').click(function(event) {
-        /* Act on the event */
-        $('html, body').animate({
-            scrollTop: '677'},
-            700, function() {
-            /* stuff to do after animation is complete */
-        });
-    });
+
+        if (widthTela > 768){
+
+            //Faz a função de rolagem em 1 segundo
+            $('html, body').animate({
+                scrollTop: '677'},
+                1000, function() {
+                /* stuff to do after animation is complete */
+            });
+        }else if (widthTela > 425){
+            
+            //Faz a função de rolagem em 1 segundo
+            $('html, body').animate({
+                scrollTop: '497'},
+                1000, function() {
+                /* stuff to do after animation is complete */
+            });
+        }
+
+    });        
 });
 
+//função para exibir o modal de login
 function abrirModalLogin(){
     $('.modal_dark').removeClass('invisible');
     $('.modal_start').removeClass('invisible');
     $('.modal_conteudo').addClass('animated zoomInUp');
 }
-
+//função  para fechar o modal de login
 function fecharModalLogin(){
     $('.modal_dark').addClass('invisible') ;
     $('.modal_start').addClass('invisible');
